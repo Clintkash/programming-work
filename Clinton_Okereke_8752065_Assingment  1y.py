a52889c3588ae40d3ef3f81dd5fef81f16ab67d7
Clinton <90466984+Clintkash@users.noreply.github.com>
2021-09-29 10:02:00 -0400

    SEC COMMIT

--- Clinton_Okereke_8752065_Assingment  1y.py
+++ Clinton_Okereke_8752065_Assingment  1y.py
@@ -2,3 +2,12 @@ print('WELCOME TO ABBYS MERCHANDZING STORE. HERE WE SELL MANY QUALITY POLOS AND 
 Type= str(input('please type in the type of Shirt you want, we have the POLO and T-SHIRT.'))
 
 Colour= str(input('Please type in the colour you want all colours are available.'))
+
+print ("Each T-shirt and polo in our store cost $9.99")  
+
+print("Please enter the quantity you will wish to buy.")
+quantity= int(input())
+
+print("The type of shirt you want is A " +str (Type))
+print('The colour is ' +str (Colour))
+print('The quantity you wish to buy is ' +str (quantity))
