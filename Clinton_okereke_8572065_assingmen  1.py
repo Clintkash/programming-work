a484ba9def96cfaa2b4f5cdb9e6accbcc100d26e
Clintkash <Cokereke2065@conestogac.on.ca>
2021-09-29 09:33:21 -0400

    FIRST

---
+++ Clinton_okereke_8572065_assingmen  1.py
@@ -0,0 +1,15 @@
+print("Welcome to addy's merchandzing store.")
+
+Type= str(input('Do you want a polo or a T-shirt? press "1" for a polo and "A" for a T-shirt.'))
+if Type .isalpha():
+    print("You have selected a polo")
+    
+elif Type .isdigit():
+    print("YOU HAVE SELECTED A 'POLO'. ")    
+
+Colour= str(input('What colour do you want?, for red colour, press "1" for white colour press "A".'))
+
+if Colour .isalpha():
+    print("You have selected a WHITE colour")
+elif Colour .isdigit():
+    print("You have selected the RED Colour.")    
