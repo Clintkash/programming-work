4fe417b20b6d89c1bfd7d30fb9a24403e5b2fc68
Clintkash <Cokereke2065@conestogac.on.ca>
2021-10-21 02:24:48 -0400

    new codes

---
+++ Assingment2.py
@@ -0,0 +1,12 @@
+print(' WELCOME TO ABBYS CLOTHING')
+Type=1
+Shoes=1
+
+Type= int(input('please press 1 for polo and 2 for T-shirt: '))
+
+if Type==1:
+    print('You have selected a POLO')
+elif Type==2:
+    print('You have selected a T-shirt')
+elif Type !=1 and Type!=2:
+    print
